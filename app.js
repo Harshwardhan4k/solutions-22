001766a72b8781f64cbd336f5d9496ac9a8cf79e
Prince Harshwardhan Kapare <76548962+Harshwardhan4k@users.noreply.github.com>
2022-04-25 01:10:13 +0530

    login working

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 // import required functions
 import {
-	getAuth,
-	onAuthStateChanged,
+  getAuth,
+  onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js";
-
-const loginBtn = document.querySelector("#nav-link-account");
 
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
-	if (user) {
-		console.log("user");
-		loginBtn.classList.add("--logged-in");
-		loginBtn.classList.remove("show-modal");
-		document.getElementById("modal").classList.add("hidden");
-		document.getElementById("overlay").classList.add("hidden");
-		loginBtn.href = "./profile/index.html";
-	} else {
-		console.log("not user");
-		loginBtn.addEventListener("click", function (e) {
-			document.getElementById("modal").classList.toggle("hidden");
-			document.getElementById("overlay").classList.toggle("hidden");
-			document.querySelector("body").classList.toggle("--lock-body");
-			console.log("clicked ");
-		});
-	}
+  if (user) {
+    console.log("user");
+    loginBtn.classList.add("--logged-in");
+    loginBtn.classList.remove("show-modal");
+	document.getElementById("modal").classList.add("hidden");
+	document.getElementById("overlay").classList.add("hidden");
+    loginBtn.href = "./profile/index.html";
+  } else {
+    console.log("not user");
+    // loginBtn.addEventListener("click", function (e) {
+    //   document.getElementById("modal").classList.toggle("hidden");
+    //   document.getElementById("overlay").classList.toggle("hidden");
+    //   document.querySelector("body").classList.toggle("--lock-body");
+    //   console.log("clicked ");
+    // });
+  }
 });
